f9c0a410c22f8aab1f7cd24da22b7a144b77a97b
Axel Wahlen <axel.wahlen@mixxt.de>
2016-05-07 15:43:10 +0200

    Complete the OhMyEmbed errors

--- lib/oh_my_embed.rb
+++ lib/oh_my_embed.rb
@@ -7,9 +7,10 @@ module OhMyEmbed
   # All OhMyEmbed errors inherits from a generic OhMyEmbed::Error class
   #
   # - OhMyEmbed::UnknownProvider
-  # - OhMyEmbed::
-  # - OhMyEmbed::
-  # - OhMyEmbed::
+  # - OhMyEmbed::ProviderNotFound
+  # - OhMyEmbed::NotFound
+  # - OhMyEmbed::PermissionDenied
+  # - OhMyEmbed::FormatNotSupported
   class Error < StandardError; end
 
   class UnknownProvider < OhMyEmbed::Error # nodoc #
@@ -21,6 +22,24 @@ module OhMyEmbed
   class ProviderNotFound < OhMyEmbed::Error # nodoc #
     def self.new(url)
       super("No provider found for given content_url (#{url}). Do you have registered the associated provider?")
+    end
+  end
+
+  class NotFound < OhMyEmbed::Error # nodoc #
+    def self.new(url)
+      super("No embed code for this content (#{url}) found")
+    end
+  end
+
+  class PermissionDenied < OhMyEmbed::Error # nodoc #
+    def self.new(url)
+      super("You don't have permissions to access the embed code for this content (#{url})")
+    end
+  end
+
+  class FormatNotSupported < OhMyEmbed::Error # nodoc #
+    def self.new(provider_name)
+      super("The provider '#{provider_name}' doesn't support json")
     end
   end
 end
