3ecc82387a8633f439e984034285d71b2bd1da5e
Axel Wahlen <axel.wahlen@mixxt.de>
2016-05-07 15:48:22 +0200

    Improve vcr config to work with the codeclimate test reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = 'fixtures/vcr_cassettes'
   config.hook_into :webmock
+  config.ignore_hosts 'codeclimate.com'
 end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
