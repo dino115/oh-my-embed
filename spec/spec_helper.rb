d1fb87f46f08c690e9b2b566f4407526e9936e03
Axel Wahlen <axel.wahlen@mixxt.de>
2016-05-08 12:59:53 +0200

    Fix vcr casette dir

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require 'webmock/rspec'
 require 'vcr'
 
 VCR.configure do |config|
-  config.cassette_library_dir = 'fixtures/vcr_cassettes'
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
   config.ignore_hosts 'codeclimate.com'
 end
